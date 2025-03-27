afa12152f9c9900f098e47e37476c6d1865b3e72
Taki Eddine <takieddineelattari@gmail.com>
2025-03-27 23:26:10 +0000

    Ajout des modeles

---
+++ models/user.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    last_login = db.Column(db.DateTime, nullable=True)
+    
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.set_password(password)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def update_last_login(self):
+        self.last_login = datetime.utcnow()
+        db.session.commit()
+        
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'username': self.username,
+            'created_at': self.created_at,
+            'last_login': self.last_login
+        }
+        
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
